8f4366c2c19bc4795bff8dd1811e45e7d2ad34b0
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-04-12 22:37:24 +0200

    added nodemon and app.use to serve static assets

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
-"use strict"
+"use strict";
 
-let http = require("http"),
-    express = require("express");
+let http = require("http");
+let express = require("express");
 
 const app = express();
+
+app.use(express.static("./public"));
+
+app.get("/", (request, response) => {
+  response.end("Hello World");
+  console.log("In Handler");
+});
+
 const server = new http.Server(app);
 const port = 8090;
 
 server.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port}`);
 });
-
