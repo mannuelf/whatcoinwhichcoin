fa2c03da4b7d84e3f85a0eda4498f21f7b1e44fe
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-04-14 17:38:49 +0200

    setup nodemon for express serve. update readme

--- server.js
+++ server.js
@@ -5,15 +5,13 @@ let express = require("express");
 
 const app = express();
 
-app.use(express.static("./public"));
+app.use(express.static("./dist"));
 
 app.get("/", (request, response) => {
-  response.end("Hello World");
-  console.log("In Handler");
 });
 
 const server = new http.Server(app);
-const port = 8090;
+const port = 8088;
 
 server.listen(port, () => {
   console.log(`Server started on port ${port}`);
