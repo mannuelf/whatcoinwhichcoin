5de815033923e6d3822d17b61dac78d4736e24fa
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-05-05 10:57:59 +0200

    refactor, got the objects we get back assigned to each coin key

--- src/components/BitStamp.vue
+++ src/components/BitStamp.vue
@@ -3,55 +3,66 @@
         <h2>Bitstamp</h2>
 
         <h3>Bitcoin</h3>
-        <p>{{ results.high }}</p>
+        <p>{{ results['btcusd'].high }}</p>
 
         <h3>Bitcoin Cash</h3>
-        <p>{{ results.high }}</p>
+        <p>{{ results['btcusd'].high }}</p>
+
+        <h3>XRP</h3>
+        <p>{{ results['xrpusd'].high }}</p>
+
+        <h3>Litecoin</h3>
+        <p>{{ results['ltcusd'].high }}</p>
 
         <h3>Etherium</h3>
-        <p>{{ results.high }}</p>
+        <p>{{ results['ethusd'].high }}</p>
         <div v-if="errors">{{ errors }}</div>
     </div>
 </template>
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-    name: "Bitstamp",
-    data() {
-        return {
-            results: [],
-            errors: []
-        }
-    },
-    beforeMount(){
-        const app = this;
+  name: "Bitstamp",
+  data() {
+    return {
+      results: {},
+      errors: []
+    };
+  },
+  beforeCreate() {
+    const app = this;
 
-        let currency = [
-            'btcusd', 'btceur', 'eurusd', 'xrpusd', 'xrpeur',
-            'xrpbtc', 'ltcusd', 'ltceur', 'ltcbtc', 'ethusd',
-            'etheur', 'ethbtc', 'bchusd', 'bcheur', 'bchbtc'
-        ];
+    let currency = ["btcusd", "bchusd", "xrpusd", "ltcusd", "ethusd"];
 
-        const conversionUrl = 'https://cors-anywhere.herokuapp.com/';
-        const url = 'https://www.bitstamp.net/api/v2/ticker/';
-    
-        for(let i = 0; i < currency.length; i++){
-                axios.get(conversionUrl+url+currency[i])
-                .then((response) => {
-                    console.log(response.data,'response');
-                    app.results = response.data;
-                })
-                .catch((e) => {
-                    let errorsNote = e
-                    this.errors.push(errorsNote)
-                    console.log(errorsNote)
-                })
-        }
-    },
-    created() {
+    const conversionUrl = "https://cors-anywhere.herokuapp.com/"; // ahppy
+    const url = "https://www.bitstamp.net/api/v2/ticker/";
+
+    let coins = {};
+    for (let i = 0; i < currency.length; i++) {
+      // console.log(currency[i])
+      // coins[currency[i]] = {
+      // 'a': '789'
+      // }
+      axios
+        .get(conversionUrl + url + currency[i])
+        .then(response => {
+          // console.log(response.data,'response');
+          coins[currency[i]] = response.data;
+          app.results[currency[i]] = response.data;
+        })
+        .catch(e => {
+          let errorsNote = e;
+          this.errors.push(errorsNote);
+          console.log(errorsNote);
+        });
     }
-}
+    console.log('appResults', app.results);
+    // console.log(coins)
+  },
+  beforeMount() {},
+  created() {}
+};
 </script>
 <style>
 
