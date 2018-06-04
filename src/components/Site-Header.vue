0c4e450ed6e5409e18dd07eb366551a6282a5daa
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-06-04 23:56:55 +0200

    site header

---
+++ src/components/Site-Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <header class="header"><h1>What Coin Which Coin</h1></header>
+    </div>    
+</template>
+
+<script>
+export default {
+  name: "site-header"
+}
+</script>
+<style lang="sass">
+.header
+    display: grid;
+    grid-template-columns: repeat(auto 1fr);
+    background-color: azure;
+    
+</style>
