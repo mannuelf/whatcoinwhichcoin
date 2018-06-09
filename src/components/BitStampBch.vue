283c98e1d70d5512a811c82240a35e462f21ec55
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-06-09 02:29:53 +0200

    add more currencies

---
+++ src/components/BitStampBch.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="block bitstamp animated fadeIn">
+        <section v-if="errored">
+            <p>Sorry something broke, please check back later</p>
+        </section>
+        <section v-else>
+          <div v-if="loading">
+            Loading...
+          </div>
+          <div v-else>
+            <a href="#" class="block__btn--std">
+              <span class="block__btn--coin">BCH</span>
+              <span class="block__btn--currency">$</span>
+              <span class="block__btn--price">{{ results.high }}</span>
+            </a> 
+          </div>
+        </section>
+    </div>
+</template>
+
+<script>
+import axios from "axios"
+export default {
+  name: "BitstampBtc",
+  data() {
+    return {
+      results: null,
+      errors: [],
+      errored: false,
+      loading: true
+    };
+  },
+  mounted() {
+    const currency = "bchusd";
+    const conversionUrl = "https://cors-anywhere.herokuapp.com/"
+    const url = "https://www.bitstamp.net/api/v2/ticker/"
+     axios
+        .get(conversionUrl + url + currency)
+        .then(response => {
+          this.results = response.data
+          console.log(results)
+        })
+        .catch(error => {
+          let errorsNote = error
+          this.errors.push(errorsNote)
+        })
+        .finally(() => {
+          this.loading = false
+        })
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <header class="site-header">
+            <h1><strong>WHATCoin</strong> WHICHCoin</h1>
+        </header>
+    </div>    
+</template>
+
+<script>
+export default {
+  name: "SiteHeader"
+}
+</script>
+
+<style lang="sass" scoped>
+h1
+    font-size: 1.4em
+    margin: 0
+    padding: 0.2em 0.5em
+</style>
