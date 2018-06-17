093f9677d4437c09f8f8758ae0b18ae51d0fff97
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-06-17 20:43:14 +0200

    cdn for css libs

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-require('../node_modules/normalize.css/normalize.css')
-require('../node_modules/milligram/dist/milligram.min.css')
-require('../node_modules/animate.css/animate.min.css')
-
 Vue.config.productionTip = false
 Vue.prototype.$appName = 'what-coin-which-coin'
 
