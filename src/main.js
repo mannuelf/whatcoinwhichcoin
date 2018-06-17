daa438832f1e71781d376bbe073d0081f1a77bb9
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-06-17 20:17:01 +0200

    upgraded vuecli

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ require('../node_modules/normalize.css/normalize.css');
 require('../node_modules/milligram/dist/milligram.min.css')
 require('../node_modules/animate.css/animate.min.css')
 
-Vue.config.productionTip = true
-Vue.prototype.$appName = 'what-coin-which-coin'
+
+Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App)
