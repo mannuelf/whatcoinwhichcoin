851fd7fe6a9ce7fb486ec71b4aa5ace4f42db6c5
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-06-17 20:37:48 +0200

    add css things

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-require('../node_modules/normalize.css/normalize.css');
+
+require('../node_modules/normalize.css/normalize.css')
 require('../node_modules/milligram/dist/milligram.min.css')
 require('../node_modules/animate.css/animate.min.css')
 
-
 Vue.config.productionTip = false
+Vue.prototype.$appName = 'what-coin-which-coin'
 
 new Vue({
   render: h => h(App)
