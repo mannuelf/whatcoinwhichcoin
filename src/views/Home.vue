2105779183c66fcf1997e507199c31651aea00cd
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-06-19 23:23:27 +0200

    vue router, fixing routes

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <div class="container">
+      <BitstampBtc />
+      <BitstampBch />
+      <BitstampEth />
+      <BitstampXrp />
+      <BitstampLtcoin />
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import BitstampBtc from '@/components/BitstampBtc.vue'
+import BitstampBch from '@/components/BitstampBch.vue'
+import BitstampEth from '@/components/BitstampEth.vue'
+import BitstampXrp from '@/components/BitstampXrp.vue'
+import BitstampLtcoin from '@/components/BitstampLtcoin.vue'
+
+export default {
+  name: 'home',
+  components: {
+    BitstampBtc,
+    BitstampBch,
+    BitstampEth,
+    BitstampXrp,
+    BitstampLtcoin
+  }
+}
+</script>
