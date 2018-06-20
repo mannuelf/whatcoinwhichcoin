8ad8ea0aba92cc95e896e071cdbe5d13605b3548
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-06-20 22:57:38 +0200

    babel-core and loader in

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   presets: [
     '@vue/app'
-  ],
-  resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js'
-    },
-    extensions: ['*', '.js', '.vue', '.json']
-  }
+  ]
 }
