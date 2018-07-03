c09dda86e0318800fa0af9c4202bdc45d004773d
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-07-03 23:15:40 +0200

    fix path mutations

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import actions from './actions.js'
-import mutations from '/mutations.js'
+import mutations from './mutations.js'
 import getters from './getters.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    
-  },
+  state: {},
   actions,
   mutations,
   getters
