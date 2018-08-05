33b32301bcee733a95ae2fd8dbf099719b0dfd10
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-08-05 22:11:58 +0200

    cache static items CSS, and then check for matches and if not matches get response from network

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,16 @@
 self.addEventListener('install', function(event) {
   console.log('[Service worker] Installing Service Worker...', event);
+  event.waitUntil(
+    caches.open('static')
+      .then(function(cache) {
+        console.log('[Service Worker] Pre-caching static assets')
+        cache.add('./index.html')
+        cache.add('https://fonts.googleapis.com/css?family=Rajdhani:300,600,700|Roboto+Mono:100,300')
+        cache.add('https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css')
+        cache.add('https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css')
+        cache.add('https://cdnjs.cloudflare.com/ajax/libs/milligram/1.3.0/milligram.min.css')
+      })
+  )
 })
 
 self.addEventListener('activate', function(event) {
@@ -8,6 +19,15 @@ self.addEventListener('activate', function(event) {
 })
 
 self.addEventListener('fetch', function(event) {
-  console.log('[Service Worker]', event);
-  event.respondWith(fetch(event.request));
+  // console.log('[Service Worker]', event);
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response
+        } else {
+          return fetch(event.request)
+        }
+      })
+  );
 })
--- src/main.js
+++ src/main.js
@@ -2,15 +2,24 @@ import Vue from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
 import store from './store/index.js'
+import 'whatwg-fetch'
 
 Vue.config.productionTip = false
 Vue.prototype.$appName = 'WHATCoinWhichCoin'
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+// for later
+// stop the default behaviour of the add to home screen prompt
+// window.addEventListener('beforeinstallprompt', function(event) {
+//   console.log('beforeinstallprompt fired')
+//   event.preventDefault()
+//   deferredPrompt = event
+//   return false
+// })
+let deferredPrompt
+
+if (!window.Promise) {
+  window.Promise = Promise
+}
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
@@ -20,3 +29,24 @@ if ('serviceWorker' in navigator) {
       console.log('serviceWorker Registered');
     })
 }
+
+if (deferredPrompt) {
+  deferredPrompt.prompt()
+  deferredPrompt.userChoice.then(function(choiceResult){
+    console.log(choiceResult.outcome)
+
+    if (choiceResult.outcome === 'dismissed') {
+      console.log('User cancelled installation')
+    } else {
+      console.log('User added to home screen')
+    }
+  })
+  deferredPrompt = null
+}
+
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
