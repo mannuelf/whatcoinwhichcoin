15102cc6a5f3627787b19d1f89f4886ee86f3126
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-10-14 18:19:38 +0200

    move config

--- config/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const port = 3000
-
-app.use(express.static('./dist'))
-app.listen(port, () => console.log(`app started on port number: ${port}`))
-
