aa20134cf15d400d50c0b763c9818d80ae44eb09
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-10-14 11:39:31 +0200

    simple express server

---
+++ config/server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = 3000
+
+app.use(express.static('dist'))
+app.listen(port, () => console.log(`app started on port number: ${port}`))
+
