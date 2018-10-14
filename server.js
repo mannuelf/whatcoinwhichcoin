e5c39f1b1c00b1497e077ef949b69e2b01ae8e73
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-10-14 18:54:23 +0200

    heroku build stuff

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const port = 3000
+const port = process.env.PORT || 5000
 
-app.use(express.static('./dist'))
+app.use(express.static(path.join(__dirname, 'dist')))
+app.get('/', (req, res) => res.render('index.html'))
 app.listen(port, () => console.log(`app started on port number: ${port}`))
 
