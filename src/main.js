57207713ae7c60bdfcb0a34b137f92fbcde8aac1
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-10-14 22:34:36 +0200

    add vue analytics

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
 import store from './store/index.js'
+import VueAnalytics from 'vue-analytics'
+
+Vue.use(VueAnalytics, {
+  id: 'UA-19677147-2'
+})
 
 Vue.config.productionTip = false
 Vue.prototype.$appName = 'WHATCoinWhichCoin'
