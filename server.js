64a9714b17d41b5459a2caecc9b74ed5012bf7ef
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-10-14 19:44:48 +0200

    set view engine

--- server.js
+++ server.js
@@ -5,5 +5,6 @@ const port = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname, 'dist')))
 app.get('/', (req, res) => res.render('index.html'))
+app.set('view engine', 'html');
 app.listen(port, () => console.log(`app started on port number: ${port}`))
 
