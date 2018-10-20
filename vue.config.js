8c15487d5932a0b331eaf03ab38284e8ea7e37f5
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-10-20 09:48:03 +0200

    icons and vue config

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  pwa: {
+    name: 'WHATCoin WHICHCoin',
+    themeColor: '#1b98e0',
+    msTileColor: '#1b98e0',
+    favicon32: 'assets/icons/favicon-32x32.png',
+    favicon16: 'assets/icons/favicon-16x16.png',
+    appleTouchIcon: 'assets/icons/icon-48.png',
+    maskIcon: 'assets/icons/icon-safari-pinned-tab.svg',
+    msTileImage: 'assets/icons/icon-144.png',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+  }
+}
