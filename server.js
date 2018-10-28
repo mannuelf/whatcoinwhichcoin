7edf187e9672425065d9cf4e7553de036234d83d
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-10-28 22:24:49 +0200

    install rollbar

--- server.js
+++ server.js
@@ -2,6 +2,16 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const Rollbar = require('rollbar')
+
+const rollbar = new Rollbar({
+  accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
 
 express()
   .use(express.static(path.join(__dirname, 'dist')))
