b581428849c69691ad4d8bccb5ebeb9b3bae5308
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2019-02-20 07:12:08 +0200

    migrate, start rebuild

---
+++ components/Coinbase.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <p>{{ results['btcusd'].high }}</p>
+    </div>
+</template>
+<script>
+import axios from "axios"
+
+export default {
+  name: "Coinbase",
+  data() {
+    return {
+      results: {},
+      errors: []
+    };
+  },
+  methods: {
+    fetchCoins() {
+      
+    }
+  },
+  mounted() {
+    this.fetchCoins()
+    console.log("mounted")
+  }
+};
+</script>
+<style>
+
+</style>
---
+++ pages/BitcoinCash.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="container bitcoin-cash">
+    <h1>This is the Bitcoin Cash page</h1>
+  </div>
+</template>
