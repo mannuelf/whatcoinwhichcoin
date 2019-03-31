0f766d901ba16e7f47dd0d3a0b066e8a2075800a
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2019-03-31 15:27:36 +0200

    update meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -72,11 +72,11 @@ module.exports = {
         url: '/',
         changefreq: 'daily',
         priority: 1,
-        lastmodISO: '2017-06-30T13:30:00.000Z'
       },
-      'about-apis',
+      '/about-apis',
       {
-        url: 'about-apis'
+        url: 'about-apis',
+        changefreq: 'daily'
       }
     ]
   },
