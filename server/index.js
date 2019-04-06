23d1dd95559aaf2bf08b60504c88775d0dc75f31
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2019-04-07 00:54:36 +0200

    clean up

--- server/index.js
+++ server/index.js
@@ -2,13 +2,6 @@ const express = require('express')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
-const Rollbar = require('rollbar')
-
-const rollbar = new Rollbar({
-  accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
-  captureUncaught: true,
-  captureUnhandledRejections: true
-})
 
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
