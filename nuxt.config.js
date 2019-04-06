fed6c9f1556e765a7aaf3a2e80bd62ae71b384c4
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2019-04-07 00:54:36 +0200

    clean up

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,58 +2,41 @@ const pkg = require('./package')
 
 module.exports = {
   mode: 'universal',
-
-  /*
-  ** Headers of the page
-  */
   head: {
     title: pkg.name,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description },
-      { name: 'google-site-verification', content: 'nKI88c4xbyXEo5ub25G2-4JqjDiggUKqIZzMF5E4aJk'},
-      { name: 'msvalidate.01', content: 'D37242BCFEACCD5D06A16A1748BF7DA1'}
+      { name: 'google-site-verification', content: 'nKI88c4xbyXEo5ub25G2-4JqjDiggUKqIZzMF5E4aJk' },
+      { name: 'msvalidate.01', content: 'D37242BCFEACCD5D06A16A1748BF7DA1' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Rajdhani:300,600,700|Roboto+Mono:100,300|VT323' },
       { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/milligram/1.3.0/milligram.min.css' },
-      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css' },
-
+      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css' }
     ]
   },
-
-  /*
-  ** Customize the progress-bar color
-  */
   loading: { color: '#1B98E0' },
-
-  /*
-  ** Global CSS
-  */
   css: ['~/assets/css/tailwind.css'],
-
-  /*
-  ** Plugins to load before mounting the App
-  */
   plugins: [
-    '~/plugins/vue-analytics.js'
+    {
+      src: '~/plugins/vue-rollbar.js',
+      ssr: true
+    },
+    {
+      src: '~/plugins/vue-analytics.js',
+      ssr: false
+    }
   ],
-
-  /*
-  ** Nuxt.js modules
-  */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
     '@nuxtjs/pwa',
     '@nuxtjs/sitemap'
   ],
-  /*
-  ** Axios module configuration
-  */
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
   },
@@ -62,7 +45,7 @@ module.exports = {
     hostname: 'https://www.whatcoinwhichcoin.com/',
     cacheTime: 1000 * 60 * 15,
     gzip: true,
-    generate: false, // Enable me when using nuxt generate
+    generate: false,
     exclude: [
       '/secret',
       '/admin/**'
@@ -81,13 +64,7 @@ module.exports = {
       }
     ]
   },
-  /*
-  ** Build configuration
-  */
   build: {
-    /*
-    ** You can extend webpack config here
-    */
     extend(config, ctx) {
       // Run ESLint on save
       if (ctx.isDev && ctx.isClient) {
