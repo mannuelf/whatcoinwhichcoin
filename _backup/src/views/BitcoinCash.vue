8d7c7fcfa1c2c5d075a629cf9984cf38245b6d9d
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2019-04-12 23:29:23 +0200

    delete it, not needed anymore

--- _backup/src/views/BitcoinCash.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="container bitcoin-cash">
-    <h1>This is the Bitcoin Cash page</h1>
-  </div>
-</template>
--- _backup/src/views/LiteCoin.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="container litecoin">
-    <h1>This is the LiteCoin page</h1>
-  </div>
-</template>
