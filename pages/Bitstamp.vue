4a631ec1572a2b8a74b6bfd3a8234cff6b5dbebd
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2019-04-17 07:33:35 +0200

    building luno and kraken pages

---
+++ pages/Bitstamp.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="home">
+    <div id="nav">
+      <BitStampBtc @click.native="goToBitcoin" />
+      <BitStampBch @click.native="goToBitcoinCash" />
+      <BitStampEth @click.native="goToEtherium" />
+      <BitStampXrp @click.native="goToXrp" />
+      <BitStampLtCoin @click.native="goToLtCoin" />
+    </div>
+    <div class="about-site">
+      <p>
+        {{ $t('home.description') }}
+      </p>
+    </div>
+    <div class="footer-note">
+      <p>
+        {{ $t('home.built-with') }}
+        <a href="https://mannuelferreira.com">
+          <strong>
+            {{ $t('home.say-hello') }}
+          </strong>
+        </a>
+        <a href="https://github.com/mannuelf/whatcoinwhichcoin">
+          <strong>
+            {{ $t('home.fork-it') }}
+          </strong>
+        </a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import BitStampBtc from '@/components/BitStampBtc.vue'
+import BitStampBch from '@/components/BitStampBch.vue'
+import BitStampEth from '@/components/BitStampEth.vue'
+import BitStampXrp from '@/components/BitStampXrp.vue'
+import BitStampLtCoin from '@/components/BitStampLtCoin.vue'
+
+export default {
+  name: 'Home',
+  head() {
+    return {
+      title: this.$t('home.title'),
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.$t('home.description')
+        }
+      ]
+    }
+  },
+  components: {
+    BitStampBtc,
+    BitStampBch,
+    BitStampEth,
+    BitStampXrp,
+    BitStampLtCoin
+  },
+  methods: {
+    goToBitcoin: function(e) {
+      // this.$router.go('/bitcoin')
+    },
+    goToBitcoinCash: function(e) {},
+    goToEtherium: function(e) {},
+    goToXrp: function(e) {},
+    goToLtCoin: function(e) {}
+  }
+}
+</script>
---
+++ pages/Luno.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="home">
+    <div id="nav">
+      <LunoTicker @click.native="goToBitcoin" />
+    </div>
+    <div class="about-site">
+      <p>
+        {{ $t('home.description') }}
+      </p>
+    </div>
+    <div class="footer-note">
+      <p>
+        {{ $t('home.built-with') }}
+        <a href="https://mannuelferreira.com">
+          <strong>
+            {{ $t('home.say-hello') }}
+          </strong>
+        </a>
+        <a href="https://github.com/mannuelf/whatcoinwhichcoin">
+          <strong>
+            {{ $t('home.fork-it') }}
+          </strong>
+        </a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import LunoTicker from '@/components/LunoTicker.vue'
+
+export default {
+  name: 'Home',
+  head() {
+    return {
+      title: this.$t('home.title'),
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.$t('home.description')
+        }
+      ]
+    }
+  },
+  components: {
+    LunoTicker
+  },
+  methods: {
+    goToBitcoin: function(e) {
+      // this.$router.go('/bitcoin')
+    },
+    goToBitcoinCash: function(e) {},
+    goToEtherium: function(e) {},
+    goToXrp: function(e) {},
+    goToLtCoin: function(e) {}
+  }
+}
+</script>
