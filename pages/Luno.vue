898f5083cc38dd96cbca854424adabe458af089a
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2019-04-17 22:34:08 +0200

    eslint no errors and tidy up

--- pages/Luno.vue
+++ pages/Luno.vue
@@ -3,26 +3,6 @@
     <div id="nav">
       <LunoTicker @click.native="goToBitcoin" />
     </div>
-    <div class="about-site">
-      <p>
-        {{ $t('home.description') }}
-      </p>
-    </div>
-    <div class="footer-note">
-      <p>
-        {{ $t('home.built-with') }}
-        <a href="https://mannuelferreira.com">
-          <strong>
-            {{ $t('home.say-hello') }}
-          </strong>
-        </a>
-        <a href="https://github.com/mannuelf/whatcoinwhichcoin">
-          <strong>
-            {{ $t('home.fork-it') }}
-          </strong>
-        </a>
-      </p>
-    </div>
   </div>
 </template>
 
