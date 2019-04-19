78b47e4009ad0a6b300e0e56633e1449cbc7d533
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2019-04-19 23:13:50 +0200

    tidy up

--- pages/Etherium.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="container etherium">
-    <h1>This is the Etherium page</h1>
-  </div>
-</template>
--- pages/Xrp.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="container xrp">
-    <h1>This is the XRP page</h1>
-  </div>
-</template>
