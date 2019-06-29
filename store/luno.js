e2fc165bd27539c85c602ac302adf88333818939
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2019-06-29 06:55:15 +0200

    cors anywhere

--- store/luno.js
+++ store/luno.js
@@ -16,17 +16,33 @@ export const mutations = {
 
 export const actions = {
   async GET_ALL({ commit }) {
-    await axios
-      .get('https://cors-anywhere.herokuapp.com/https://api.mybitx.com/api/1/tickers')
-      .then(response => {
-        if (response.status === 200) {
-          commit('SET_ALL', response.data)
-        }
-      })
-      .catch(err => {
-        // eslint-disable-next-line no-console
-        commit('ERROR', err)
-        console.log(err)
-      })
+    try {
+      await axios
+        .get('https://api.mybitx.com/api/1/tickers', {
+          method: 'get',
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers':
+              'Origin, X-Requested-With, Content-Type, Accept'
+          }
+        })
+        .then(response => {
+          console.log('response', response.data)
+          console.log(response.data)
+          console.log(response.status)
+          console.log(response.statusText)
+          console.log(response.headers)
+          if (response.status === 200) {
+            commit('SET_ALL', response.data)
+          }
+        })
+        .catch(err => {
+          // eslint-disable-next-line no-console
+          commit('ERROR', err)
+          console.log(err)
+        })
+    } catch (err) {
+      console.log(err)
+    }
   }
 }
