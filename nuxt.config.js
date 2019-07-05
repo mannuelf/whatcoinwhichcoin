8cecd570bef9753443e6014ce5ba111d84e9d5cd
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2019-07-05 22:43:41 +0200

    move manifest to nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -123,5 +123,54 @@ module.exports = {
         })
       }
     }
+  },
+  manifest: {
+    name: "WHAT Coin WHICH Coin",
+    short_name: "WCWC",
+    start_url: "/",
+    scope: ".",
+    display: "fullscreen",
+    orientation: "portrait-primary",
+    background_color: "#1B98E0",
+    theme_color: "#1B98E0",
+    description: "Crypto currency price dashboard",
+    dir: "ltr",
+    icons: [
+      {
+        "src": "/assets/icons/android-icon-48x48.png",
+        "type": "image/png",
+        "sizes": "48x48"
+      },
+      {
+        "src": "/assets/icons/android-icon-96x96.png",
+        "type": "image/png",
+        "sizes": "96x96"
+      },
+      {
+        "src": "/assets/icons/android-icon-144x144.png",
+        "type": "image/png",
+        "sizes": "144x144"
+      },
+      {
+        "src": "/assets/icons/android-icon-192x192.png",
+        "type": "image/png",
+        "sizes": "192x192"
+      },
+      {
+        "src": "/assets/icons/android-icon-256x256.png",
+        "type": "image/png",
+        "sizes": "256x256"
+      },
+      {
+        "src": "/assets/icons/android-icon-384x384.png",
+        "type": "image/png",
+        "sizes": "384x384"
+      },
+      {
+        "src": "/assets/icons/android-icon-512x512.png",
+        "type": "image/png",
+        "sizes": "512x512"
+      }
+    ]
   }
 }
