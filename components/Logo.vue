73d6fb75c7a4d85db32e337adb3aa09ec4f7b7c4
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2019-07-06 08:54:27 +0200

    remove tailwind - sorry bro. add post headers for luno on express server fix logo on header

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,7 +1,11 @@
 <template>
-  <figure class="coin-logo">
-    <img :src="'/logos/'+exchange+'.png'" :alt="exchange">
-  </figure>
+  <div class="coin-logo">
+    <img
+      :src="'/logos/'+exchange+'.png'"
+      :alt="exchange"
+      class="coin-logo__image"
+    >
+  </div>
 </template>
 <script>
 export default {
@@ -31,6 +35,21 @@ export default {
   }
 }
 </script>
+<style lang="sass" scoped>
+.coin-logo
+  z-index: 10
+  background-color: #fdfff7
+  display: inline-block
+  height: 40px
+  margin: 0
+  overflow: hidden
+  padding: 10px
+  position: absolute
+  right: 1em
+  top: 2.9em
+  width: 100px
+  border-radius: 4px 4px
 
-<style>
+  &__image
+    width: 100%
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -11,36 +11,111 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'google-site-verification', content: 'nKI88c4xbyXEo5ub25G2-4JqjDiggUKqIZzMF5E4aJk' },
+      {
+        name: 'google-site-verification',
+        content: 'nKI88c4xbyXEo5ub25G2-4JqjDiggUKqIZzMF5E4aJk'
+      },
       { name: 'msvalidate.01', content: 'D37242BCFEACCD5D06A16A1748BF7DA1' },
       { name: 'yandex-verification', content: '026fe23883783f5c' },
       { name: 'msapplication-TileColor', content: '#0090df' },
-      { name: 'msapplication-TileImage', content: '/layoutsicons/ms-icon-144x144.png' }
+      {
+        name: 'msapplication-TileImage',
+        content: '/layoutsicons/ms-icon-144x144.png'
+      }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/icons/favicon.ico' },
-      { rel: "apple-touch-icon", sizes: "57x57", href: "/icons/apple-icon-57x57.png" },
-      { rel: "apple-touch-icon", sizes: "60x60", href: "/icons/apple-icon-60x60.png" },
-      { rel: "apple-touch-icon", sizes: "72x72", href: "/icons/apple-icon-72x72.png" },
-      { rel: "apple-touch-icon", sizes: "76x76", href: "/icons/apple-icon-76x76.png" },
-      { rel: "apple-touch-icon", sizes: "114x114", href: "/icons/apple-icon-114x114.png" },
-      { rel: "apple-touch-icon", sizes: "120x120", href: "/icons/apple-icon-120x120.png" },
-      { rel: "apple-touch-icon", sizes: "144x144", href: "/icons/apple-icon-144x144.png" },
-      { rel: "apple-touch-icon", sizes: "152x152", href: "/icons/apple-icon-152x152.png" },
-      { rel: "apple-touch-icon", sizes: "180x180", href: "/icons/apple-icon-180x180.png" },
-      { rel: "icon", type: "image/png", sizes: "192x192", href: "/icons/android-icon-192x192.png" },
-      { rel: "icon", type: "image/png", sizes: "32x32", href: "/icons/favicon-32x32.png" },
-      { rel: "icon", type: "image/png", sizes: "96x96", href: "/icons/favicon-96x96.png" },
-      { rel: "icon", type: "image/png", sizes: "16x16", href: "/icons/favicon-16x16.png" },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Rajdhani:300,600,700|Roboto+Mono:100,300|VT323' },
-      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css'}
+      {
+        rel: 'apple-touch-icon',
+        sizes: '57x57',
+        href: '/icons/apple-icon-57x57.png'
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '60x60',
+        href: '/icons/apple-icon-60x60.png'
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '72x72',
+        href: '/icons/apple-icon-72x72.png'
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '76x76',
+        href: '/icons/apple-icon-76x76.png'
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '114x114',
+        href: '/icons/apple-icon-114x114.png'
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '120x120',
+        href: '/icons/apple-icon-120x120.png'
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '144x144',
+        href: '/icons/apple-icon-144x144.png'
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '152x152',
+        href: '/icons/apple-icon-152x152.png'
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/icons/apple-icon-180x180.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '192x192',
+        href: '/icons/android-icon-192x192.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/icons/favicon-32x32.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '96x96',
+        href: '/icons/favicon-96x96.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/icons/favicon-16x16.png'
+      },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css'
+      },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Rajdhani:300,600,700|Roboto+Mono:100,300|VT323'
+      },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css'
+      }
     ]
   },
   loading: { loading: true, color: '#1B98E0' },
   styleResources: {
     scss: ['./assets/sass/*.sass']
   },
-  css: ['~/assets/sass/app.sass', '~/assets/css/tailwind.css'],
+  css: ['~/assets/sass/app.sass'],
   plugins: [
     {
       src: '~/plugins/vue-analytics.js',
@@ -156,56 +231,56 @@ module.exports = {
     ]
   },
   manifest: {
-    name: "WHAT Coin WHICH Coin",
-    short_name: "WCWC",
-    start_url: "/",
-    scope: "/",
-    display: "fullscreen",
-    orientation: "portrait-primary",
-    background_color: "#0090df",
-    theme_color: "#0090df",
-    description: "Crypto currency price dashboard",
-    dir: "ltr",
+    name: 'WHAT Coin WHICH Coin',
+    short_name: 'WCWC',
+    start_url: '/',
+    scope: '/',
+    display: 'fullscreen',
+    orientation: 'portrait-primary',
+    background_color: '#0090df',
+    theme_color: '#0090df',
+    description: 'Crypto currency price dashboard',
+    dir: 'ltr',
     icons: [
       {
-        "src": "/icons/icon-72x72.png",
-        "sizes": "72x72",
-        "type": "image/png"
+        src: '/icons/icon-72x72.png',
+        sizes: '72x72',
+        type: 'image/png'
       },
       {
-        "src": "/icons/icon-96x96.png",
-        "sizes": "96x96",
-        "type": "image/png"
+        src: '/icons/icon-96x96.png',
+        sizes: '96x96',
+        type: 'image/png'
       },
       {
-        "src": "/icons/icon-128x128.png",
-        "sizes": "128x128",
-        "type": "image/png"
+        src: '/icons/icon-128x128.png',
+        sizes: '128x128',
+        type: 'image/png'
       },
       {
-        "src": "/icons/icon-144x144.png",
-        "sizes": "144x144",
-        "type": "image/png"
+        src: '/icons/icon-144x144.png',
+        sizes: '144x144',
+        type: 'image/png'
       },
       {
-        "src": "/icons/icon-152x152.png",
-        "sizes": "152x152",
-        "type": "image/png"
+        src: '/icons/icon-152x152.png',
+        sizes: '152x152',
+        type: 'image/png'
       },
       {
-        "src": "/icons/icon-192x192.png",
-        "sizes": "192x192",
-        "type": "image/png"
+        src: '/icons/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
       },
       {
-        "src": "/icons/icon-384x384.png",
-        "sizes": "384x384",
-        "type": "image/png"
+        src: '/icons/icon-384x384.png',
+        sizes: '384x384',
+        type: 'image/png'
       },
       {
-        "src": "/icons/icon-512x512.png",
-        "sizes": "512x512",
-        "type": "image/png"
+        src: '/icons/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
       }
     ]
   }
