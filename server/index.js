b37d6219d7620a98d3e77e5561c606655f635a4d
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2019-07-30 01:48:39 +0200

    refactor, make generic component and created action and api call

--- server/index.js
+++ server/index.js
@@ -25,10 +25,7 @@ async function start() {
   }
 
   app.use((req, res, next) => {
-    res.header(
-      'Access-Control-Allow-Origin',
-      '*'
-    )
+    res.header('Access-Control-Allow-Origin', '*')
     res.header(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept'
--- store/bitstamp.js
+++ store/bitstamp.js
@@ -1,13 +1,34 @@
 import axios from 'axios'
 
+const url = 'https://www.bitstamp.net/api/v2/ticker/'
+const conversionUrl = 'https://cors-anywhere.herokuapp.com/'
+
 export const state = () => ({
-  list: [],
+  coins: {
+    bch: '',
+    btc: '',
+    eth: '',
+    ltc: '',
+    xrp: ''
+  },
   error: ''
 })
 
 export const mutations = {
-  SET_ALL(state, coins) {
-    state.list = coins
+  SET_BCH(state, payload) {
+    state.coins.bch = payload
+  },
+  SET_BTC(state, payload) {
+    state.coins.btc = payload
+  },
+  SET_ETH(state, payload) {
+    state.coins.eth = payload
+  },
+  SET_LTC(state, payload) {
+    state.coins.ltc = payload
+  },
+  SET_XRP(state, payload) {
+    state.coins.xrp = payload
   },
   ERROR(state, error) {
     state.error = error
@@ -15,23 +36,21 @@ export const mutations = {
 }
 
 export const actions = {
-  async GET_ALL({ commit }) {
-    try {
-      await axios
-        .get('https://www.bitstamp.net/api/v2/ticker/', {
-          method: 'get'
-        })
-        .then(response => {
-          if (response.status === 200) {
-            commit('SET_ALL', response.data)
-          }
-        })
-        .catch(err => {
-          // eslint-disable-next-line no-console
-          commit('ERROR', err)
-        })
-    } catch (err) {
-      console.log(err)
-    }
+  async GET_BTC({ commit }) {
+    const currency = 'btcusd'
+    console.log('trying api call >>')
+    await axios
+      .get(conversionUrl + url + currency)
+      .then(response => {
+        const coin = response.data
+        console.log('coin:', coin)
+        commit('SET_BTC', coin)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+      .finally(() => {
+        this.loading = false
+      })
   }
 }
