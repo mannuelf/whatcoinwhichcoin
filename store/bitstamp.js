61cbb894286ee5d1ea8fe61d43436d4d15c33975
Mannuel Ferreira <mannuel.ferreir@gmail.com>
2020-01-05 00:28:29 +0100

    refactor coin price ticker for new data. need new error handling.

--- store/bitstamp.js
+++ store/bitstamp.js
@@ -1,13 +1,5 @@
 import axios from 'axios'
 
-const urls = [
-  'https://www.bitstamp.net/api/v2/ticker/btcusd',
-  'https://www.bitstamp.net/api/v2/ticker/bchusd',
-  'https://www.bitstamp.net/api/v2/ticker/ethusd',
-  'https://www.bitstamp.net/api/v2/ticker/ltcusd',
-  'https://www.bitstamp.net/api/v2/ticker/xrpusd'
-]
-
 export const state = () => ({
   coin: {
     exchange: 'bitstamp',
@@ -47,33 +39,48 @@ export const mutations = {
 
 export const actions = {
   async GET_COINS({ commit }) {
-    await urls.map(async url => {
-      const response = await axios.get(url)
-      if (response.status === 200) {
-        commit('SET_END', false)
-        const respData = await response.data
-        const coinName = url.substr(url.length - 6)
-        console.log(respData)
-        switch (coinName) {
-          case 'btcusd':
-            commit('SET_BTC', respData)
-            break
-          case 'bchusd':
-            commit('SET_BCH', respData)
-            break
-          case 'ethusd':
-            commit('SET_ETH', respData)
-            break
-          case 'ltcusd':
-            commit('SET_LTC', respData)
-            break
-          case 'xrpusd':
-            commit('SET_XRP', respData)
-            break
-          default:
-            console.log('No coins matched')
+    const urls = [
+      'https://www.bitstamp.net/api/v2/ticker/btcusd',
+      'https://www.bitstamp.net/api/v2/ticker/bchusd',
+      'https://www.bitstamp.net/api/v2/ticker/ethusd',
+      'https://www.bitstamp.net/api/v2/ticker/ltcusd',
+      'https://www.bitstamp.net/api/v2/ticker/xrpusd'
+    ]
+    try {
+      await urls.map(async url => {
+        const response = await axios.get(url)
+        if (response.status === 200) {
+          const respData = await response.data
+          const coinName = url.substr(url.length - 6)
+          switch (coinName) {
+            case 'btcusd':
+              commit('SET_BTC', respData)
+              commit('SET_END', false)
+              break
+            case 'bchusd':
+              commit('SET_BCH', respData)
+              commit('SET_END', false)
+              break
+            case 'ethusd':
+              commit('SET_ETH', respData)
+              commit('SET_END', false)
+              break
+            case 'ltcusd':
+              commit('SET_LTC', respData)
+              commit('SET_END', false)
+              break
+            case 'xrpusd':
+              commit('SET_XRP', respData)
+              commit('SET_END', false)
+              break
+            default:
+              console.log('No coins matched')
+          }
+          commit('SET_END', false)
         }
-      }
-    })
+      })
+    } catch (error) {
+      commit('SET_ERROR_MESSAGE', error)
+    }
   }
 }
